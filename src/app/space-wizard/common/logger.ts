51829843ae0fa378378a63691596ec80124856ed
mitchharpur <mitchharpur@metajunction.com>
2017-03-20 12:52:34 -0400

    corrected logger contract

--- src/app/space-wizard/common/logger.ts
+++ src/app/space-wizard/common/logger.ts
@@ -4,7 +4,7 @@
     warning?:boolean,
     error?:boolean,
     info?:boolean,
-    inner?any
+    inner?:any
   }
   export interface ILoggerDelegate{
     (options:string|ILogEntry):void
@@ -13,7 +13,24 @@
 
   export function getLogger(name:string,instance:number):ILoggerDelegate
   {
-      return function logger(options:ILogEntry) {
+      function log(entry:ILogEntry)
+      {
+        let method = "log";
+        if (entry.error === true) {
+          method = "error";
+        }
+        if (entry.warning === true) {
+          method = "warn";
+        }
+        if (entry.info === true) {
+          method = "info";
+        }
+        let typeStyle="background:linear-gradient(#444, #333); border-radius:15px;padding:4px;color:lime;font-style:italic;border-left:solid 0px orangered;padding:3px;padding-left:10px;padding-right:10px"
+        let instanceStyle="background:linear-gradient(#444, #333);color:orangered; border-radius:10px;padding:3px;margin:3px 0;";
+        let messageStyle="background:linear-gradient(#444, #333);color:white; border-radius:10px;padding:3px 10px;";
+        console[method].apply(null, [`%c${name}%c ${instance} %c${entry.message || ""}`,typeStyle,instanceStyle,messageStyle]);
+      };
+      return function logger(options:string|ILogEntry) {
         let entry = { message: "" };
         if (typeof options === "string") {
           entry.message = options || "";
@@ -21,19 +38,6 @@
         else {
           Object.assign(entry, options);
         }
-        let method = "log";
-        if (options.error === true) {
-          method = "error";
-        }
-        if (options.warning === true) {
-          method = "warn";
-        }
-        if (options.info === true) {
-          method = "info";
-        }
-        let typeStyle="background:linear-gradient(#444, #333); border-radius:15px;padding:4px;color:lime;font-style:italic;border-left:solid 0px orangered;padding:3px;padding-left:10px;padding-right:10px"
-        let instanceStyle="background:linear-gradient(#444, #333);color:orangered; border-radius:10px;padding:3px;margin:3px 0;";
-        let messageStyle="background:linear-gradient(#444, #333);color:white; border-radius:10px;padding:3px 10px;";
-        console[method].apply(null, [`%c${name}%c ${instance} %c${entry.message || ""}`,typeStyle,instanceStyle,messageStyle]);
+        log(entry);
       };
   }
