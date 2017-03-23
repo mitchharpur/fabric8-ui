a730f1900ec83fa9b8d32088fc2cf3b1dc48ee69
mitchharpur <mitchharpur@metajunction.com>
2017-03-23 11:52:38 -0400

    svc erro handler

--- src/app/space-wizard/services/concrete/fabric8-forge.service.ts
+++ src/app/space-wizard/services/concrete/fabric8-forge.service.ts
@@ -2,12 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { Observable, Observer } from 'rxjs/Rx';
-import { IForgeRequest, IForgePayload, IForgeResponse, ForgeService,ForgeCommands } from '../contracts/forge-service';
+import { IForgeRequest, IForgePayload, IForgeResponse, ForgeService, ForgeCommands } from '../contracts/forge-service';
 
 import { LoggerFactory, ILoggerDelegate } from '../../common/logger';
-
-
-
 
 @Injectable()
 export class Fabric8ForgeService extends ForgeService {
@@ -24,6 +21,7 @@ export class Fabric8ForgeService extends ForgeService {
       }
     }
   };
+
   constructor(private http: Http, loggerFactory: LoggerFactory) {
     super()
     let logger = loggerFactory.createLoggerDelegate(this.constructor.name, Fabric8ForgeService.instanceCount++);
@@ -32,9 +30,10 @@ export class Fabric8ForgeService extends ForgeService {
     }
     this.log(`New instance...`);
   }
+
   private handleError(err): Observable<any> {
     let errMessage = err.message ? err.message : err.status ? `${err.status} - ${err.statusText}` : 'Server Error';
-    //this.log({ message: errMessage, inner: err, error: true })
+    this.log({ message: errMessage, inner: err, error: true })
     return Observable.throw(new Error(errMessage));
   }
   private GetCommand(url: string): Observable<IForgeResponse> {
@@ -42,97 +41,91 @@ export class Fabric8ForgeService extends ForgeService {
       let headers = new Headers();
       //headers.append(...) e.g ('Content-Type', 'application/json');
       //TODO: auth token
+      this.log(`forge GET : ${url}`);
       this.http.get(url, headers)
         .map((response) => {
-          let payload: IForgePayload = response.json();
-          //console.dir(payload);
-          return { payload: payload };
+          let forgeResponse: IForgeResponse = { payload: response.json() };
+          this.log(`forge GET response : ${url}`);
+          console.dir(forgeResponse.payload);
+          return forgeResponse;
         })
-        .catch(this.handleError)
+        .catch((err)=>this.handleError(err))
         .subscribe((response: IForgeResponse) => {
           observer.next(response);
           observer.complete();
         })
     });
   }
-  private PostCommand(url: string, payload: any): Observable<IForgeResponse> {
+
+  private PostCommand(url: string, body: any): Observable<IForgeResponse> {
     return Observable.create((observer: Observer<IForgeResponse>) => {
       let headers = new Headers();
       //headers.append('Content-Type', 'application/json');
-      this.http.post(url, payload, headers)
+      this.log(`forge POST : ${url}`);
+      console.dir(body)
+      this.http.post(url, body, headers)
         .map((response) => {
-          let payload: IForgePayload = response.json();
-          //console.dir(payload);
-          return { payload: payload };
+          let forgeResponse: IForgeResponse = { payload: response.json() };
+          this.log(`forge POST response : ${url}`);
+          console.dir(forgeResponse.payload);
+          return forgeResponse;
         })
-        .catch(this.handleError)
+        .catch((err)=>this.handleError(err))
         .subscribe((response: IForgeResponse) => {
           observer.next(response);
           observer.complete();
         })
     });
   }
-  ExecuteCommand(options: IForgeRequest = { command: { name: "empty" } }): Observable<IForgeResponse> {
-    switch (options.command.name) {
-      case ForgeCommands.forgeStarter: {
-        let parameters: any = options.command.parameters;
-        switch (parameters.workflow.step.name) {
-          case "begin":
-            {
-              let url = `${this.config.forge.api.url.https}/forge/commands/obsidian-new-project`;
-              this.log(`Executing forge ${url}`);
-              return this.GetCommand(url);
-            }
-          case "next":
-            {
-              let url = `${this.config.forge.api.url.https}/forge/commands/obsidian-new-project/next`;
-              let data = parameters.data || {};
-              console.dir(data);
-              let payload = parameters.data;
-              payload.stepIndex = parameters.workflow.step.index || 1;
-              this.log(`posting to forge ${url}`);
-              console.dir(payload)
-              return this.PostCommand(url, payload);
-            }
-          case "validate":
-            {
-              let url = `${this.config.forge.api.url.https}/forge/commands/obsidian-new-project/validate`;
-              let data = parameters.data || {};
-              console.dir(data);
-              let payload = parameters.data;
-              payload.stepIndex = parameters.workflow.step.index || 1;
-              this.log(`posting to forge ${url}`);
-              console.dir(payload)
-              return this.PostCommand(url, payload);
-            }
-          case "execute":
-            {
-              let url = `${this.config.forge.api.url.https}/forge/commands/obsidian-new-project/execute`;
-              let data = parameters.data || {};
-              console.dir(data);
-              let payload = parameters.data;
-              payload.stepIndex = parameters.workflow.step.index || 1;
-              this.log(`posting to forge ${url}`);
-              console.dir(payload)
-              return this.PostCommand(url, payload);
-            }
-          default: {
-            this.log({ message: `invalid forge command:${options.command.name} step:${parameters.workflow.step.name}`, error: true });
-            return createEmptyResponse();
-          }
-        }
 
+  ExecuteCommand(request: IForgeRequest = { command: { name: "empty" } }): Observable<IForgeResponse> {
+    switch (request.command.name) {
+      case ForgeCommands.forgeStarter: {
+        request.command.forgeCommandName = "obsidian-new-project";
+        return this.forgeWorkflowCommandRequest(request);
+      }
+      case ForgeCommands.forgeQuickStart: {
+        request.command.forgeCommandName = "obsidian-new-quickstart";
+        return this.forgeWorkflowCommandRequest(request);
       }
       default: {
-        return createEmptyResponse();
+        return Observable.empty();
       }
     }
   }
-}
 
-function createEmptyResponse(): Observable<IForgeResponse> {
-  return Observable.create((observer: Observer<IForgeResponse>) => {
-    observer.next({});
-    observer.complete();
-  })
+  private forgeWorkflowCommandRequest(request: IForgeRequest): Observable<IForgeResponse> {
+    let parameters: any = request.command.parameters;
+    let forgeCommandName = request.command.forgeCommandName;
+    switch (parameters.workflow.step.name) {
+      case "begin":
+        {
+          let url = `${this.config.forge.api.url.https}/forge/commands/${forgeCommandName}`;
+          return this.GetCommand(url);
+        }
+      case "next":
+        {
+          let url = `${this.config.forge.api.url.https}/forge/commands/${forgeCommandName}/next`;
+          let body = parameters.data || {};
+          body.stepIndex = parameters.workflow.step.index || 1;
+          return this.PostCommand(url, body);
+        }
+      case "validate":
+        {
+          let url = `${this.config.forge.api.url.https}/forge/commands/${forgeCommandName}/validate`;
+          let body = parameters.data || {};
+          return this.PostCommand(url, body);
+        }
+      case "execute":
+        {
+          let url = `${this.config.forge.api.url.https}/forge/commands/${forgeCommandName}/execute`;
+          let body = parameters.data;
+          return this.PostCommand(url, body);
+        }
+      default: {
+        this.log({ message: `invalid forge command:${request.command.name} step:${parameters.workflow.step.name}`, error: true });
+        return Observable.empty();
+      }
+    }
+  }
 }
